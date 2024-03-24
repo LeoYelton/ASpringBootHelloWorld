6262acd093cbd05be67127aebe17ad25f08262a1
LeoYelton <yc53yelixing@163.com>
2024-03-24 15:51:06 +0800

    feat/fix: Implement the executeAlgorithm method in

---
+++ src\main\java\org\example\Algorithm.java
@@ -0,0 +1,16 @@
+<Algorithm.java>
+<![CDATA[
+package org.example;
+
+import org.springframework.stereotype.Component;
+import org.example.Fibonacci;
+
+@Component
+public class Algorithm extends HelloWorld {
+public void executeAlgorithm(double[] data) {
+long[] fibonacciSequence = Fibonacci.getFibonacciSequence(10);
+// Perform calculations using the Fibonacci sequence and the financial data
+}
+}
+]]>
+</Algorithm.java>
